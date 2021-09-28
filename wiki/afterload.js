e3e979976d9e5e12ddff98b258ec27bd7d8727de
fabrialeespinach@outlook.com <fabrialeespinach@outlook.com>
2021-09-27 21:41:56 -0300

    Testing

--- wiki/afterload.js
+++ wiki/afterload.js
@@ -1,1 +1,2 @@
+___.head = document.getElementsByTagName('head')
 ___.head.innerHTML = ___.head.innerHTML + "<meta property='og:title' content='Example Title'/><meta property='og:url' content='https://esmindustrygame.github.io/wiki/'/>"
