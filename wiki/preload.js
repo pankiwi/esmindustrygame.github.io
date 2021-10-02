045da4e38e12e442853a631d2b6257219480cf01
fabrialeespinach@outlook.com <fabrialeespinach@outlook.com>
2021-10-02 20:44:03 -0300

    saving now light & color mode on localStorage, Copper page, new functions and images

--- wiki/preload.js
+++ wiki/preload.js
@@ -4,6 +4,44 @@ else if(subDirs > 3){subDirs = 3;}
 var dirString = '';
 for(let i = 0; i<subDirs; i++){dirString = dirString + '../';}
 
+(() => {
+  let lightingMode = localStorage.getItem('lightingMode');
+  console.log(lightingMode)
+  if(lightingMode === 'false' || typeof lightingMode === 'undefined'){
+    //Dark
+    document.documentElement.style.setProperty('--font-color',' #fff');
+    document.documentElement.style.setProperty('--background-light-light','var(--background-dark-light-light)');
+    document.documentElement.style.setProperty('--background-light','var(--background-dark-light)');
+    document.documentElement.style.setProperty('--background','var(--background-dark)');
+    document.documentElement.style.setProperty('--background-hover','var(--background-dark-hover)');
+    document.documentElement.style.setProperty('--shadow-color','#505050');
+  }else{
+    //light
+    document.documentElement.style.setProperty('--font-color',' #000');
+    document.documentElement.style.setProperty('--background-light-light','var(--background-white-light-light)');
+    document.documentElement.style.setProperty('--background-light','var(--background-white-light)');
+    document.documentElement.style.setProperty('--background','var(--background-white)');
+    document.documentElement.style.setProperty('--background-hover','var(--background-white-hover)');
+    document.documentElement.style.setProperty('--shadow-color','#d1d1d1');
+  }
+
+  let colorMode = localStorage.getItem('colorMode');
+  console.log(colorMode)
+  if(colorMode){
+    document.documentElement.style.setProperty('--main-light-light', `var(--${colorMode}-light-light)`);
+    document.documentElement.style.setProperty('--main-light', `var(--${colorMode}-light)`);
+    document.documentElement.style.setProperty('--main', `var(--${colorMode})`);
+    document.documentElement.style.setProperty('--main-hover', `var(--${colorMode}-hover)`);
+  }else{
+    document.documentElement.style.setProperty('--main-light-light', `var(--orange-light-light)`);
+    document.documentElement.style.setProperty('--main-light', `var(--orange-light)`);
+    document.documentElement.style.setProperty('--main', `var(--orange)`);
+    document.documentElement.style.setProperty('--main-hover', `var(--orange-hover)`);
+  }
+})();
+
+
+
 const discordInvite ='https://discord.gg/dgXjVpxWnn';
 var ___ = {}
 ___.content = document.getElementById('content');
@@ -11,7 +49,7 @@ ___.content = document.getElementById('content');
 <head>
   <meta charset='UTF-8'>
   <title>Mindustry Wiki Español</title>
-  <link rel='stylesheet' href='styles/style.css'>
+  <link rel='stylesheet' href='${dirString}styles/style.css'>
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu|Ubuntu+Mono">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
@@ -53,7 +91,7 @@ ___.content = document.getElementById('content');
           <li class='header-element color-li'><input class='color-button' id='color-green' type='button'></li>
           <li class='header-element color-li'><input class='color-button' id='color-blue' type='button'></li>
           <li class='header-element color-li'><input class='color-button' id='color-purple' type='button'></li>
-          <li class='header-element color-li'><input class='color-button' id='color-black' type='button'></li>
+          <li class='header-element color-li'><input class='color-button' id='color-neutral' type='button'></li>
         </ul>
       </li>
     </ul>
@@ -80,6 +118,7 @@ ___.configIsActive = false;
 for(button of (document.getElementsByClassName('color-button'))){
   let colorName = button.id.slice(6, button.id.length);
   button.addEventListener('click', () => {
+    localStorage.setItem('colorMode', colorName);
     document.documentElement.style.setProperty('--main-light-light', `var(--${colorName}-light-light)`);
     document.documentElement.style.setProperty('--main-light', `var(--${colorName}-light)`);
     document.documentElement.style.setProperty('--main', `var(--${colorName})`);
@@ -89,22 +128,23 @@ for(button of (document.getElementsByClassName('color-button'))){
 ___.ilumination = document.getElementById('ilumination');
 ___.ilumination.addEventListener('change', () => {
   if(parseInt(___.ilumination.value) >= 127){
+    localStorage.setItem('lightingMode','true');
     document.documentElement.style.setProperty('--font-color',' #000');
-    document.documentElement.style.setProperty('--background','#fff');
+    document.documentElement.style.setProperty('--background-light-light','var(--background-white-light-light)');
+    document.documentElement.style.setProperty('--background-light','var(--background-white-light)');
+    document.documentElement.style.setProperty('--background','var(--background-white)');
+    document.documentElement.style.setProperty('--background-hover','var(--background-white-hover)');
     document.documentElement.style.setProperty('--shadow-color','#d1d1d1');
   }else{
+    localStorage.setItem('lightingMode','false');
     document.documentElement.style.setProperty('--font-color',' #fff');
-    document.documentElement.style.setProperty('--background','var(--dark-gray)');
+    document.documentElement.style.setProperty('--background-light-light','var(--background-dark-light-light)');
+    document.documentElement.style.setProperty('--background-light','var(--background-dark-light)');
+    document.documentElement.style.setProperty('--background','var(--background-dark)');
+    document.documentElement.style.setProperty('--background-hover','var(--background-dark-hover)');
     document.documentElement.style.setProperty('--shadow-color','#505050');
   }
 })
-
-
-
-
-
-
-
 
 /**Referencia al div content */
 const content = document.getElementById('content');
@@ -122,11 +162,14 @@ const writeContent = (html) => {if(typeof html === 'string'){content.innerHTML =
  * - data.id: (String) Id
  * - data.class: (String) Clases
  * - data.style: (String) Estilo (.css)
+ * - data.code: (Boolean) Si el texto es código
+ * - data.codeBig: (Boolean) Si el texto es una página de código
  * @returns {String} <span>
 */
 const span = (data) => {
   if(!data){console.error('Datos del span no definidos');}
   else if(typeof data === 'string'){
+    data = data.replaceAll('\n','<br>')
     return `<span>${data}</span>`
   }else if(typeof data === 'object'){
     if(!data.content){data.content = '';}
@@ -137,6 +180,23 @@ const span = (data) => {
     if(typeof data.id !== 'string'){console.error('data.id debe ser tipo "string"');}
     if(!data.style){data.style = '';}
     if(typeof data.style !== 'string'){console.error('data.style debe ser tipo "string"');}
+
+    if(typeof data.code !== 'boolean' && typeof data.code !== 'undefined'){console.error('data.code debe ser tipo "Boolean"')}
+    if(data.code){
+      data.class = data.class + ' codeText ';
+      data.content = data.content.replaceAll('<','&lt');
+      data.content = data.content.replaceAll('>','&gt');
+      data.content = data.content.replaceAll('&ltbr&gt','<br>');
+    }
+
+    if(typeof data.codeBig !== 'boolean' && typeof data.codeBig !== 'undefined'){console.error('data.codeBig debe ser tipo "Boolean"')}
+    if(data.codeBig){
+      data.class = data.class + ' codeTextBigger ';
+      data.content = data.content.replaceAll('<','&lt');
+      data.content = data.content.replaceAll('>','&gt');
+      data.content = data.content.replaceAll('&ltbr&gt','<br>');
+    }
+    data.content = data.content.replaceAll('\n','<br>')
     return `<span id='${data.id}' class='${data.class}' style='${data.style}'>${data.content}</span>`
   } 
 }
@@ -160,17 +220,19 @@ const a = (data) => {
     if(typeof data.content !== 'string'){console.error('data.content debe ser tipo "string"');}
     if(!data.href){data.href = '';}
     if(typeof data.href !== 'string'){console.error('data.href debe ser tipo "string"');}
-    if(typeof data.styledText !== 'boolean' && typeof data.styledText !== 'undefined'){console.error('data.styledText debe ser tipo "Boolean"')}
-    if(data.styledText === false || typeof data.styledText === 'undefined'){data.styledText = 'color: var(--main);';}
-    else{data.styledText = 'text-decoration: none;'}
     if(!data.class){data.class = '';}
     if(typeof data.class !== 'string'){console.error('data.class debe ser tipo "string"');}
+    
+    if(typeof data.styledText !== 'boolean' && typeof data.styledText !== 'undefined'){console.error('data.styledText debe ser tipo "Boolean"')}
+    if(data.styledText === true || typeof data.styledText === 'undefined'){data.class = data.class + ' aLink ';}
+    else{data.class = data.class + ' noDecoration'}
+    
     if(!data.id){data.id = '';}
     if(typeof data.id !== 'string'){console.error('data.id debe ser tipo "string"');}
     if(!data.style){data.style = '';}
     if(typeof data.newTab !== 'boolean'&& typeof data.newTab !== 'undefined'){console.error('data.newTab debe ser tipo "Boolean"')}
     if(!data.newTab){data.newTab = '';}
-    else{data.newTab = 'target="blank"'}
+    else{data.newTab = 'target="_blank"'}
     if(typeof data.style !== 'string'){console.error('data.style debe ser tipo "string"');}
     return `<a id='${data.id}' class='${data.class}' style='${data.styledText}${data.style}' href='${data.href}' ${data.newTab}>${data.content}</a>`
   } 
@@ -252,17 +314,69 @@ const img = (data) => {
     if(typeof data.id !== 'string'){console.error('data.id debe ser tipo "string"');}
     if(!data.style){data.style = '';}
     if(typeof data.style !== 'string'){console.error('data.style debe ser tipo "string"');}
-    if(typeof data.center !== 'boolean'&& typeof data.center !== 'undefined'){console.error('data.center debe ser tipo "Boolean"')}
-    if(!data.center){data.center = '';}
-    else{data.center = 'display:block;margin:10px auto;';}
+    if(typeof data.center !== 'boolean' && typeof data.center !== 'undefined'){console.error('data.center debe ser tipo "Boolean"')}
+    if(data.center){data.class = data.class + ' imgCenter '}
     if(typeof data.url !== 'boolean'&& typeof data.url !== 'undefined'){console.error('data.url debe ser tipo "Boolean"')}
     if(!data.url){
-      return `<img id='${data.id}' class='${data.class}' style='${data.center}${data.style}' src='${dirString}content/images/${data.img}' width='${data.width}' height='${data.height}'>`
-    }else{return `<img id='${data.id}' class='${data.class}' style='${data.center}${data.style}' src='${data.img}' width='${data.width}' height='${data.height}'>`}
+      return `<img id='${data.id}' class='${data.class}' style='${data.style}' src='${dirString}content/images/${data.img}' width='${data.width}' height='${data.height}'>`
+    }else{return `<img id='${data.id}' class='${data.class}' style='${data.style}' src='${data.img}' width='${data.width}' height='${data.height}'>`}
   }else{console.error('data debe ser tipo "string" o "object"{}');}
   
 }
 /**Crea un salto de linea*/
 const br = () => {
   return '<br>'
+}
+
+/**Crea una tabla de objetos (Minerales) 
+ * @param {Object} data Datos:
+ * - data.content.explosividad: (String) Porcentaje de explosividad
+ * - data.content.inflamabiidad: (String) Porcentaje de inflamabilidad
+ * - data.content.radioactividad: (String) Porcentaje de radioactividad
+ * - data.content.cargaElectrica: (String) Porcentaje de carga electrica
+ * - data.content.nota: (String) Nota que trae consigo el objeto
+ * - data.content.nombreInterno: (String) Nombre interno del objeto en el juego
+ * - data.content.color: (String) Color en hex del objeto
+ * - data.content.dureza: (String) Dureza del objeto
+ * @return {String} <table>
+*/
+const tableObjetos = (data) => {
+  if(typeof data !== 'object'){return console.error('data debe ser tipo "Object"')}
+  else if(typeof data.content !== 'object'){return console.error('data.content debe ser tipo "Object"')}
+  let float;
+  if(data.float === 'right'){float = 'float: right;'}else{float = 'float: left;'}
+  let tbl = `<div class="table-div" style='${float}'><table> <tr><th>Propiedad</th><th>Valor</th></tr>`;
+  
+  if(typeof data.content.descripcion !== 'string' && typeof data.content.descripcion !== 'undefined'){console.error('data.content.descripcion debe ser tipo "String"')}
+  if((typeof data.content.explosividad !== 'number' && typeof data.content.explosividad !== 'string') && typeof data.content.explosividad !== 'undefined'){console.error('data.content.explosividad debe ser tipo "String" o "Int"')}
+  if((typeof data.content.inflamabilidad !== 'number' && typeof data.content.inflamabilidad !== 'string') && typeof data.content.inflamabilidad !== 'undefined'){console.error('data.content.inflamabilidad debe ser tipo "String" o "Int"')}
+  if((typeof data.content.radioactividad !== 'number' && typeof data.content.radioactividad !== 'string') && typeof data.content.radioactividad !== 'undefined'){console.error('data.content.radioactividad debe ser tipo "String" o "Int"')}
+  if((typeof data.content.cargaElectrica !== 'number' && typeof data.content.cargaElectrica !== 'string') && typeof data.content.cargaElectrica !== 'undefined'){console.error('data.content.cargaElectrica debe ser tipo "String" o "Int"')}
+  if(typeof data.content.nota !== 'string' && typeof data.content.nota !== 'undefined'){console.error('data.content.nota debe ser tipo "String"')}
+  if(typeof data.content.nombreInterno !== 'string' && typeof data.content.nombreInterno !== 'undefined'){console.error('data.content.nombreInterno debe ser tipo "String"')}
+  if(typeof data.content.color !== 'string' && typeof data.content.color !== 'undefined'){console.error('data.content.color debe ser tipo "String"')}
+  if(typeof data.content.dureza !== 'string' && typeof data.content.dureza !== 'undefined'){console.error('data.content.dureza debe ser tipo "String"')}
+
+  if(data.content.descripcion){if(!data.content.descripcion.endsWith('.')){data.content.descripcion = data.content.descripcion + '.'}}
+
+
+  if(typeof data.content.descripcion !== 'undefined'){tbl = tbl + `<tr><td>Descripción</td><td>${data.content.descripcion}</td></tr>`}
+  if(typeof data.content.explosividad !== 'undefined'){tbl = tbl + `<tr><td>Explosividad</td><td>${data.content.explosividad}%</td></tr>`}
+  if(typeof data.content.inflamabilidad !== 'undefined'){tbl = tbl + `<tr><td>Inflamabilidad</td><td>${data.content.inflamabilidad}%</td></tr>`}
+  if(typeof data.content.radioactividad !== 'undefined'){tbl = tbl + `<tr><td>Radioactividad</td><td>${data.content.radioactividad}%</td></tr>`}
+  if(typeof data.content.cargaElectrica !== 'undefined'){tbl = tbl + `<tr><td>Carga eléctrica</td><td>${data.content.cargaElectrica}%</td></tr>`}
+  if(typeof data.content.nombreInterno !== 'undefined'){
+    data.content.nombreInterno = data.content.nombreInterno.replaceAll('<','&lt');
+    data.content.nombreInterno = data.content.nombreInterno.replaceAll('>','&gt');
+    data.content.nombreInterno = data.content.nombreInterno.replaceAll('&ltbr&gt','<br>');
+    tbl = tbl + `<tr><td>Nombre Interno</td><td><span class='codeText'>${data.content.nombreInterno}</span></td></tr>`
+  }
+  if(typeof data.content.color !== 'undefined'){
+    
+    
+    tbl = tbl + `<tr><td>Color</td><td><span class='codeText'>${data.content.color}</spans><div class='color-square', style='background-color: ${data.content.color};'></div></td></tr>`
+  }
+  if(typeof data.content.dureza !== 'undefined'){tbl = tbl + `<tr><td>Dureza</td><td>${data.content.dureza}</td></tr>`}
+  if(typeof data.content.nota !== 'undefined'){tbl = tbl + `<tr><td>Nota</td><td>${data.content.nota}</td></tr>`}
+  return tbl + '</table></div>';
 }
